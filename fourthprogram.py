97e8ea2d167a3b1b136db6d2396c4e547f87145c
Hina-Amin98 <166119200+Hina-Amin98@users.noreply.github.com>
2024-04-05 17:17:11 +0500

    operators

---
+++ fourthprogram.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+sum = num1 + num2
+print("Sum:", sum)
+
+difference = num1 - num2
+print("Difference:", difference)
+
+product = num1 * num2
+print("Product:", product)
+
+quotient = num1 / num2
+print("Quotient:", quotient)
