50ff13ed01707a8d50440df4af22a6c43022fe87
Hina-Amin98 <166119200+Hina-Amin98@users.noreply.github.com>
2024-04-25 17:42:05 +0500

    swapping

---
+++ swappingprogram.py
@@ -0,0 +1,13 @@
+chaicup = 10
+greentea = 20
+empty = 0
+print ("values before swapping")
+print ("chaicup:", chaicup)
+print ("greentea:", greentea)
+
+empty = chaicup
+chaicup = greentea
+greentea = empty
+print ("values after swapping")
+print ("chaicup:", chaicup)
+print ("greentea:", greentea)
