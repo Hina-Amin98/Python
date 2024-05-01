dc10599b7dc740a9c63d507971064d7c6ebb0273
Hina-Amin98 <166119200+Hina-Amin98@users.noreply.github.com>
2024-05-01 17:01:05 +0500

    operators and arrays

---
+++ operatorthrougharray.py
@@ -0,0 +1,11 @@
+number = [4, 8, 10, 12]
+sum = number[0] + number[2]
+print("Sum:", sum)
+difference = number[0] - number[3]
+print("Difference:", difference)
+
+product = number[1] * number[2]
+print("Product:", product)
+
+quotient = number[2] / number[2]
+print("Quotient:", quotient)
