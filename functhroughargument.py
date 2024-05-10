35c04e5945c167cf343a6f1b14c7637afd10428f
Hina-Amin98 <166119200+Hina-Amin98@users.noreply.github.com>
2024-05-10 12:37:49 +0500

    elif and arguments

---
+++ functhroughargument.py
@@ -0,0 +1,10 @@
+def calculate(num1, num2, op):
+    if(op == '+'):
+        return num1 + num2
+    elif(op == '-'):
+        return num1 - num2
+    elif(op == '*'):
+        return num1 * num2
+    elif(op == '/'):
+        return num1 / num2
+print(calculate(8, 6, "+"))    
